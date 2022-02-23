248760d6269749d18d03cba18dcb4dfa3bed1bc7
atinglis314 <abigail.t.inglis@gmail.com>
2022-02-22 21:31:52 -0600

    may need to change data types?

--- scripts/app_trader_script.sql
+++ scripts/app_trader_script.sql
@@ -1,5 +1,15 @@
+-- app_store_apps full data for reference
 select *
 from app_store_apps;
 
+-- play_store_apps full data for reference
 select *
 from play_store_apps;
+
+--group by test
+SELECT *
+FROM app_store_apps
+GROUP BY content_rating
+
+--https://www.youtube.com/watch?v=gRwgDEm8kKs
+--link to data type change how-to ^
