36a84337383a1e7fdb1c584567894a69ac6ce76f
Aurora Larson <roryvick93@gmail.com>
2022-02-24 21:33:22 -0600

    worked more on exploring the data and google

--- scripts/app_store_backup.sql
+++ scripts/app_store_backup.sql
@@ -1,4 +1,4 @@
-SELECT *
+/*SELECT *
 FROM play_store_apps;
 SELECT *
 FROM app_store_apps;
@@ -9,3 +9,47 @@ ORDER BY AVG(price) DESC;
 SELECT price, name
 FROM play_store_apps
 GROUP BY price 
+SELECT p.name, p.price
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name
+WHERE a.rating > 4 AND p.rating > 4;
+SELECT
+	DISTINCT p.name AS app_name,
+	p.rating AS play_rating,
+	a.rating AS app_rating,
+	TO_NUMBER(p.price, 'L99.99') AS play_price,
+	a.price AS app_price,
+	install_count,
+	a.primary_genre,
+	p.genres
+FROM play_store_apps AS p
+INNER JOIN app_store_apps AS a
+ON p.name = a.name
+WHERE p.rating > 4.0 AND a.rating > 4.0
+ORDER BY install_count DESC
+LIMIT 15;*/
+SELECT
+	DISTINCT a.name,
+	a.price,
+	ROUND((a.rating + p.rating)/2,1) AS combined_rating,
+	(CAST(a.review_count as int) + p.review_count)/2 AS combined_review_count
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name
+WHERE ROUND((a.rating + p.rating)/2,2) > 4.5
+ORDER BY (CAST(a.review_count as int) + p.review_count)/2 DESC
+
+
+
+
+
+
+
+
+
+
+
+
+
+
