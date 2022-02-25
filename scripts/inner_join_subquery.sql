d7e2c94ed2bef09b04133bf2b6ea20a62bba45ca
Rob Schulteis <robschulteis@gmail.com>
2022-02-24 20:42:47 -0600

    inner join madness

---
+++ scripts/inner_join_subquery.sql
@@ -0,0 +1,15 @@
+SELECT 
+	DISTINCT a.name,
+	(a.price + CAST(REPLACE(p.price,'$','') AS float)/2) as avg_price,
+	ROUND((a.rating + p.rating)/2,1) AS combined_rating,
+	(CAST(a.review_count as int) + p.review_count)/2 AS combined_review_count,
+	CAST(REPLACE(REPLACE(p.install_count,'+',''),',','') as float) AS install_count
+FROM app_store_apps AS a
+ INNER JOIN 
+ (SELECT DISTINCT name,rating, review_count, install_count, price
+ 	FROM play_store_apps) AS p
+ON a.name = p.name
+WHERE ROUND((a.rating + p.rating)/2,2) > 4.5 
+	AND CAST(REPLACE(REPLACE(p.install_count,'+',''),',','') as float) > 10000000
+ORDER BY (CAST(a.review_count as int) + p.review_count)/2 DESC, CAST(REPLACE(REPLACE(p.install_count,'+',''),',','') as float) DESC
+
